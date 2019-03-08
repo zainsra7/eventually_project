19cba3f84338db3fa6de4f8e12076d26d8cada25
ZainSra <m.zain.ul.islam@gmail.com>
2019-03-08 00:52:42 +0000

    Modified Index and base, Urls and View Setup
    Using CSS Flexbox modified index and base page, also updated urls.py and view.py

--- eventually/urls.py
+++ eventually/urls.py
@@ -4,5 +4,13 @@ from eventually import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'about/$', views.about, name='about'),
-
+    url(r'^profile/$',views.profile, name='profile'),
+    url(r'^register/$',views.register, name='register'),
+    url(r'^dashboard/$',views.dashboard, name='dashboard'),
+    url(r'^login/$',views.login, name='login'),
+    url(r'^logout/$',views.logout, name='logout'),
+    url(r'^contact/$',views.contact, name='contact'),
+    url(r'^search/',views.search,name='search'),
+    url(r'^host/$',views.host, name='host'),
+    url(r'^event/',views.event, name='event')
 ]
