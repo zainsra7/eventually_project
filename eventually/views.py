eb29b5193d8002ae132da03fc05859dee1bc3f0e
ZainSra <m.zain.ul.islam@gmail.com>
2019-03-09 22:20:06 +0000

    Image Validation in register view

--- eventually/views.py
+++ eventually/views.py
@@ -1,8 +1,9 @@
 import cloudinary
 from django.http import HttpResponse
 from django.shortcuts import render
-from PIL import Image
 from eventually.forms import UserForm, UserProfileForm
+from django.contrib.staticfiles.templatetags.staticfiles import static
+
 
 cloudinary.config(
     cloud_name='eventually',
@@ -43,6 +44,9 @@ def register(request):
     # Successful registration check
     registered = False
 
+    # To display error if the uploaded picture is not valid
+    image_error = ""
+
     if request.method == 'POST':
 
         user_form = UserForm(data=request.POST)
@@ -50,44 +54,38 @@ def register(request):
 
         # Check if forms are valid
         if user_form.is_valid() and profile_form.is_valid():
-
             # Save user's form data to database
-            user = user_form.save()
-
-            # Hash password and save user object
-            user.set_password(user.password)
-            user.save()
+            user = user_form.save(commit=False)
 
             # Save user profile form date to database
-            profile = profile_form.save(commit=False)
-            profile.user = user
+            user_profile = profile_form.save(commit=False)
+            save_profile = True
 
-            # Add profile picture to UserProfile model if provided by user
             if 'profile_pic' in request.FILES:
-                # Verifying if the uploaded file is valid image (using Pillow)
-                image = Image.open(request.FILES['profile_pic'])
-                try:
-                    image.verify()
-
+                picture = request.FILES['profile_pic']
+                if '.jpg' in picture.name or '.png' in picture.name:
                     # Uploading Photo to Cloudinary in "user_photo" folder with id of username
                     response = cloudinary.uploader.upload(request.FILES['profile_pic'],
                                                       folder="user_photo/",
                                                       public_id=user.username)
-                    profile.profile_pic = response['secure_url']
+                    user_profile.profile_pic = response['secure_url']
+                else:
+                    save_profile = False
+                    image_error = "Invalid Image File Type!"
+            # In Case there is no uploaded image, use default ranog one
+            else:
+                user_profile.profile_pic = static('images/rango.jpg')
 
-                    # Save the UserProfile model instance
-                    profile.save()
-
-                    # Indicate that registration was successful
-                    registered = True
-
-                except Exception:
-                    print('Invalid image')
-
+            if save_profile:
+                # Hash password and save user object
+                user.set_password(user.password)
+                user.save()
+                user_profile.user = user
+                user_profile.save()
+                registered = True # Indicate that Registration was successful
         else:
             # Print problems to the terminal in case of invalid forms
             print(user_form.errors, profile_form.errors)
-
     else:
         # Render blank form if not HTTP POST
         user_form = UserForm()
@@ -96,7 +94,8 @@ def register(request):
     # Render template depending on the context.
     return render(request, 'eventually/register.html', {'user_form': user_form,
                                                         'profile_form': profile_form,
-                                                        'registered': registered})
+                                                        'registered': registered,
+                                                        'image_error': image_error})
 
 
 def logout(request):
