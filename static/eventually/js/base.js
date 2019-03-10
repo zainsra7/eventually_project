1623390cab5105c9fe1fd605925d0c842f78a3b5
ZainSra <m.zain.ul.islam@gmail.com>
2019-03-10 15:35:03 +0000

    Login, Logout, Register and Profile working
    Need sam to add his email + session code so that profile pic of every user is updated.
    
    + I will add toast and view messages afterwards and captcha

--- static/eventually/js/base.js
+++ static/eventually/js/base.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $(".sidenav").sidenav();
+    $('.tooltipped').tooltip();
 
     $("#login-btn").click(function(){
       $("#login-btn").hide();
@@ -9,5 +10,9 @@ $(document).ready(function(){
     $("#cancel-login-btn").click(function(){
       $("#login-form").hide("slow");
       $("#login-btn").show();
+    });
+
+    $("#login_send").click(function(){
+      $("#login-form").submit();
     });
 });
