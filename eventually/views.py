184784f1918220da746a3267cf52779db6e84fb5
Samuel Agbede <2370844a@student.gla.ac.uk>
2019-03-15 00:16:47 +0000

    About to fix register bug

--- eventually/views.py
+++ eventually/views.py
@@ -1,15 +1,17 @@
 import cloudinary
+import qrcode
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from eventually.forms import UserForm, UserProfileForm, ProfileForm, EventForm, EventImageForm
 from django.contrib.staticfiles.templatetags.staticfiles import static
-from eventually.models import UserProfile
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 
-from django.core.mail import send_mail, EmailMultiAlternatives
+from django.core.mail import\
+    send_mail, EmailMultiAlternatives
 
 from eventually.forms import UserForm, UserProfileForm
 import random
@@ -31,7 +33,7 @@ def index(request):
     profile_pic = "/static/images/pickachu.png"
     if request.user.is_authenticated():
         profile_pic = UserProfile.objects.get(user=request.user).profile_pic
-    context_dict = {'events': events, "profile_pic" : profile_pic}
+    context_dict = {'events': events, "profile_pic": profile_pic}
     response = render(request, 'eventually/index.html', context=context_dict)
     return response
 
@@ -65,7 +67,7 @@ def host(request):
         event_image_form = EventImageForm(data=request.POST)
 
         if event_form.is_valid() and event_image_form.is_valid():
-            event = event_form.save(commit=False) # Get Event Object
+            event = event_form.save(commit=False)  # Get Event Object
 
             event_created = True
 
@@ -73,8 +75,8 @@ def host(request):
                 image = request.FILES['image']
                 if '.jpg' in image.name or '.png' in image.name:
                     response = cloudinary.uploader.upload(request.FILES['image'],
-                                                        folder="event_photo/",
-                                                        public_id=event.id)
+                                                          folder="event_photo/",
+                                                          public_id=event.id)
                     event.image = response['secure_url']
                 else:
                     event_created = False
@@ -88,11 +90,14 @@ def host(request):
         # Return a blank form
         event_form = EventForm()
         event_image_form = EventImageForm()
-    return render(request,'eventually/host.html',{"event_form": event_form, "event_image_form" : event_image_form, "image_error": image_error, "event_created": event_created})
+    return render(request, 'eventually/host.html',
+                  {"event_form": event_form, "event_image_form": event_image_form, "image_error": image_error,
+                   "event_created": event_created})
 
 
 def event(request):
     return render(request, 'eventually/event.html', {})
+
 
 @login_required
 def user_profile(request):
@@ -125,8 +130,8 @@ def user_profile(request):
                     if '.jpg' in picture.name or '.png' in picture.name:
                         # Uploading Photo to Cloudinary in "user_photo" folder with id of username, it updates and replaces old version of file
                         response = cloudinary.uploader.upload(request.FILES['profile_pic'],
-                                                            folder="user_photo/",
-                                                            public_id=user.username)
+                                                              folder="user_photo/",
+                                                              public_id=user.username)
                         profile.profile_pic = response['secure_url']
                         profile_pic = response['secure_url']
                     else:
@@ -138,7 +143,8 @@ def user_profile(request):
                     user.save(update_fields=['password'])
                     # Save user profile form date to database
                     profile.user = user
-                    profile.save(update_fields=['user','profile_pic']) # Only update "user" and "profile_pic" fields of UserProfile instance
+                    profile.save(update_fields=['user',
+                                                'profile_pic'])  # Only update "user" and "profile_pic" fields of UserProfile instance
             except (User.DoesNotExist, UserProfile.DoesNotExist) as e:
                 return HttpResponseRedirect(reverse('index'))
         else:
@@ -151,9 +157,9 @@ def user_profile(request):
 
     # Render template depending on the context.
     return render(request, 'eventually/user_profile.html', {'user_form': user_form,
-                                                        'profile_form': profile_form,
-                                                        'profile_update': profile_update,
-                                                        'image_error': image_error})
+                                                            'profile_form': profile_form,
+                                                            'profile_update': profile_update,
+                                                            'image_error': image_error})
 
 
 # Sign Up flow 1. Show user form for registration 2. Validate form upon submission and return errors if any 3. If
@@ -183,10 +189,10 @@ def register(request):
                 users = User.objects.get(email=email_address)
             except MultipleObjectsReturned:
                 return render(request, 'eventually/register.html', {'user_form': user_form,
-                                                        'profile_form': profile_form,
-                                                        'registered': registered,
-                                                        'image_error': image_error,
-                                                        'email_error': 'Email address is already taken'})
+                                                                    'profile_form': profile_form,
+                                                                    'registered': registered,
+                                                                    'image_error': image_error,
+                                                                    'email_error': 'Email address is already taken'})
             except ObjectDoesNotExist:
                 # Save user's form data to database
                 user = user_form.save(commit=False)
@@ -240,62 +246,13 @@ def register(request):
                                                         'registered': registered,
                                                         'image_error': image_error})
 
+
 @login_required
 def user_logout(request):
     # Since we know the user is logged in, we can now just log them out.
     logout(request)
     # Take the user back to the homepage
     return HttpResponseRedirect(reverse('index'))
-
-
-def user_login(request):
-    # If the request is a HTTP POST, try to pull out the relevant information
-    if request.method == 'POST':
-        # Gather the username and password provided by the user.
-        # This information is obtained from the login form.
-        # We use request.POST.get('<variable>') as opposed
-        # to request.POST['<variable>'], because the
-        # request.POST.get('<variable>') returns None if the
-        # value does not exist, while request.POST['<variable>']
-        # will raise a KeyError exception
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        # Use Django's machinery to attemp to see if the username/password
-        # combination is valid - a User object is returned if it is
-        user = authenticate(username=username, password=password)
-
-        profile = UserProfile.objects.get(user=user)
-
-        # If we have a User object, the details are correct.
-        # If None (Python's way of representing the absence of a value), no user
-        # with matching crendentials was found
-        if user:
-            if profile.approved is False:
-                # save user profile id to session
-                request.session['profile_id'] = user.id
-                request.session['password'] = password
-                return HttpResponseRedirect(reverse('account_confirmation'))
-            # Is the account active? It could have been disabled
-            elif user.is_active:
-                # If the account is valid and active, we can log the user in
-                # We'll send the user back to the homepage
-                login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-            else:
-                # An inactive account was used - no logging in!
-                return HttpResponse("Your Eventually account is disabled.")
-        else:
-            # Bad login details were provided, So we can't log the user in
-            print("Invalid login details: {0}, {1}".format(username, password))
-            return HttpResponse("Invalid login details supplied.")
-
-    # The request is not HTTP POST, so display the login form
-    # This scenario would most likely be a HTTP GET
-    else:
-        # No context variables to pass to the template system, hence the
-        # blank dictionary object
-        return render(request, 'eventually/index.html', {})
 
 
 def about(request):
@@ -342,7 +299,6 @@ def account_confirmation(request):
 
 def contact(request):
     return HttpResponse("Contact us page showing Team behind Eventually")
-
 
 
 # Apologies for the ambiguity between forgot_password and password reset. Forgot_password is the first page
@@ -399,3 +355,97 @@ def password_reset(request):
                           {'error': 'Email address is incorrect'})
 
     return render(request, 'eventually/forgot_password_confirmation.html', {})
+
+
+def send_mail_api(username, email, ver_code):
+    print(username, email, ver_code)
+    subject, from_email, to = 'Verification Code', 'events@eventually.com', email
+    text_content = "Dear %s, \nPlease enter your verification code: %s" % (username, ver_code)
+    html_content = "Dear %s, \nPlease enter your verification code: %s" % (username, ver_code)
+    message = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    message.attach_alternative(html_content, "text/html")
+    message.send()
+
+
+def send_events_qr_code(username, email, event_name, event_date, event_venue):
+    image = qrcode.make("Name : Sam. Event : Event 2")
+    imageByteArray = io.BytesIO()
+    image.save(imageByteArray, format='PNG')
+    image_modified = imageByteArray.getvalue()
+
+    subject, from_email, to = 'Ticket for %s ' % event_name, 'events@eventually.com', email
+    text_content = "Dear %s, Thanks for registering. Please contact customer care for ticket" % username
+    html_content = '<p>Dear %s, Thanks for registering for %s. Please find attached your special QR invitation. ' \
+                   'Scan ' \
+                   'this at the venue.<p> ' \
+                   '<br>' \
+                   '<br>' \
+                   '<strong>Event details</strong> <br>' \
+                   'Event Name : %s <br>' \
+                   'Event Date : %s <br>' \
+                   'Event Venue : %s <br>' % (username, event_name, event_name, event_date, event_venue)
+    message = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    message.attach_alternative(html_content, "text/html")
+
+    message.attach('sam.png', image_modified, 'image/png')
+    message.send()
+
+
+def send_mail_forgot_password(email, ver_code):
+    send_mail("Reset your password", "Please enter this code to reset to your password : %s" % ver_code,
+              "events@eventually.com", [email], fail_silently=True)
+
+
+def user_login(request):
+    # If the request is a HTTP POST, try to pull out the relevant information
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        # We use request.POST.get('<variable>') as opposed
+        # to request.POST['<variable>'], because the
+        # request.POST.get('<variable>') returns None if the
+        # value does not exist, while request.POST['<variable>']
+        # will raise a KeyError exception
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Use Django's machinery to attemp to see if the username/password
+        # combination is valid - a User object is returned if it is
+        user = authenticate(username=username, password=password)
+
+        try:
+            profile = UserProfile.objects.get(user=user)
+        except ObjectDoesNotExist:
+            return render(request, 'eventually/index.html', {'error': 'No user matches the details inputted'})
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching crendentials was found
+        if user:
+            if profile.approved is False:
+                # save user profile id to session
+                request.session['profile_id'] = user.id
+                request.session['password'] = password
+                return HttpResponseRedirect(reverse('account_confirmation'))
+            # Is the account active? It could have been disabled
+            elif user.is_active:
+                # If the account is valid and active, we can log the user in
+                # We'll send the user back to the homepage
+                login(request, user)
+                send_events_qr_code(username, user.email, "Fresh events!", event_date="16/12/2019",
+                                    event_venue="The Moon!")
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your Eventually account is disabled.")
+        else:
+            # Bad login details were provided, So we can't log the user in
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+
+    # The request is not HTTP POST, so display the login form
+    # This scenario would most likely be a HTTP GET
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object
+        return render(request, 'eventually/index.html', {})
